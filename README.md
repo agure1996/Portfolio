f5b1e21f27e8485a1522fca08158aa227b2602c5
A <abbasgure@gmail.com>
2024-04-14 08:14:21 +0100

    Fix nav links, experience section complete

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Portfolio
+Portfolio website containing previous job experiences and hosting personal projects
---
+++ index.js
@@ -0,0 +1,14 @@
+/* Use Strict */
+
+const toggleMenu = () => {
+
+    const menu = document.querySelector('.menu-links')
+    const icon = document.querySelector('.hamburger-icon')
+
+
+
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+toggleMenu();
